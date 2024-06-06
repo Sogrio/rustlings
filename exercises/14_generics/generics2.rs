b5a496ed0af91aaf6c7517ffcd6c68527559d133
Sogrio <95721424+Sogrio@users.noreply.github.com>
2024-06-06 10:48:30 +0200

    Update generics2.rs

--- exercises/14_generics/generics2.rs
+++ exercises/14_generics/generics2.rs
@@ -6,11 +6,13 @@
 // Execute `rustlings hint generics2` or use the `hint` watch subcommand for a
 // hint.
 
+// On définit une structure générique Wrapper qui peut envelopper n'importe quel type T.
 struct Wrapper<T> {
     value: T,
 }
 
 impl<T> Wrapper<T> {
+    // On implémente une méthode new générique qui crée une instance de Wrapper pour n'importe quel type T.
     pub fn new(value: T) -> Self {
         Wrapper { value }
     }
@@ -22,11 +24,13 @@ mod tests {
 
     #[test]
     fn store_u32_in_wrapper() {
+        // On vérifie si Wrapper peut contenir un u32.
         assert_eq!(Wrapper::new(42).value, 42);
     }
 
     #[test]
     fn store_str_in_wrapper() {
+        // On vérifie si Wrapper peut contenir une chaîne de caractères (&str).
         assert_eq!(Wrapper::new("Foo").value, "Foo");
     }
 }
