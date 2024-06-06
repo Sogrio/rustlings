f84ac7e2de3c8eb8525c7df24faccb7a577227d8
Sogrio <95721424+Sogrio@users.noreply.github.com>
2024-06-06 10:47:15 +0200

    Update errors6.rs

--- exercises/13_error_handling/errors6.rs
+++ exercises/13_error_handling/errors6.rs
@@ -9,6 +9,8 @@
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a
 // hint.
 
+// I AM NOT DONE
+
 use std::num::ParseIntError;
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
@@ -22,17 +24,17 @@ impl ParsePosNonzeroError {
     fn from_creation(err: CreationError) -> ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
-
-    // TODO: add another error conversion function here.
-    fn from_parse(err: ParseIntError) -> ParsePosNonzeroError {
+    // Ajout d'une autre fonction de conversion d'erreur ici.
+    // Cette fonction convertit une ParseIntError en ParsePosNonzeroError.
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError {
         ParsePosNonzeroError::ParseInt(err)
     }
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
-    // TODO: change this to return an appropriate error instead of panicking
-    // when `parse()` returns an error.
-    let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parse)?;
+    // Changement pour retourner une erreur appropriée au lieu de paniquer
+    // lorsque `parse()` renvoie une erreur.
+    let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parseint)?;
     PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
 }
 
