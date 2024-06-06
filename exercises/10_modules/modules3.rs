d580e9a65511e22d90534aa622577ac668225502
Sogrio <95721424+Sogrio@users.noreply.github.com>
2024-06-06 10:37:49 +0200

    Update modules3.rs

--- exercises/10_modules/modules3.rs
+++ exercises/10_modules/modules3.rs
@@ -8,12 +8,15 @@
 // Execute `rustlings hint modules3` or use the `hint` watch subcommand for a
 // hint.
 
-// TODO: Complete this use statement
+// On utilise l'instruction 'use' pour importer SystemTime et UNIX_EPOCH du module std::time
 use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() {
+    // On obtient le temps actuel et on calcule la durée depuis l'époque UNIX
     match SystemTime::now().duration_since(UNIX_EPOCH) {
+        // Si l'opération réussit, on affiche le nombre de secondes depuis l'époque UNIX
         Ok(n) => println!("1970-01-01 00:00:00 UTC was {} seconds ago!", n.as_secs()),
+        // Si l'opération échoue (ce qui signifie que le temps actuel est avant l'époque UNIX), on génère une panique
         Err(_) => panic!("SystemTime before UNIX EPOCH!"),
     }
 }
