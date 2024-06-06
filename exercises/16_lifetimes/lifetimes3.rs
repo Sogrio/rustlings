57e08005d7930d1c07530918d596c17ebbcd5873
Sogrio <95721424+Sogrio@users.noreply.github.com>
2024-06-06 10:50:25 +0200

    Update lifetimes3.rs

--- exercises/16_lifetimes/lifetimes3.rs
+++ exercises/16_lifetimes/lifetimes3.rs
@@ -5,6 +5,7 @@
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a
 // hint.
 
+// On définit une structure Book qui prend une référence avec une durée de vie 'a pour l'auteur et le titre.
 struct Book<'a> {
     author: &'a str,
     title: &'a str,
@@ -13,7 +14,9 @@ struct Book<'a> {
 fn main() {
     let name = String::from("Jill Smith");
     let title = String::from("Fish Flying");
+    // On crée une instance de Book avec des références aux chaînes de caractères.
     let book = Book { author: &name, title: &title };
 
+    // On affiche le titre et l'auteur du livre.
     println!("{} by {}", book.title, book.author);
 }
