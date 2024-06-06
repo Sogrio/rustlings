8f949eb0ff478ed5e50ead2ff82c98d9ecb9fc3c
Sogrio <95721424+Sogrio@users.noreply.github.com>
2024-06-06 10:58:36 +0200

    Update macros4.rs

--- exercises/21_macros/macros4.rs
+++ exercises/21_macros/macros4.rs
@@ -3,17 +3,21 @@
 // Execute `rustlings hint macros4` or use the `hint` watch subcommand for a
 // hint.
 
-#[rustfmt::skip]
+#[rustfmt::skip] // Indique à Rustfmt de ne pas formater ce bloc de code
+
+// Définition d'une macro `my_macro` avec deux patterns de correspondance
 macro_rules! my_macro {
+    // Aucun argument passé à la macro
     () => {
-        println!("Check out my macro!");
+        println!("Check out my macro!"); // Affiche un message
     };
+    // Correspondance pour un argument passé à la macro
     ($val:expr) => {
-        println!("Look at this other macro: {}", $val);
+        println!("Look at this other macro: {}", $val); // Affiche un message avec la valeur de l'argument
     }
 }
 
 fn main() {
-    my_macro!();
-    my_macro!(7777);
+    my_macro!(); // Appel de la macro sans argument
+    my_macro!(7777); // Appel de la macro avec un argument
 }
